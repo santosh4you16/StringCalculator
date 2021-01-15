28d98de54bcac8ad4d0f2c1c6ffd3779d3d7d91f
santosh sah <sk.santosh99111@gmail.com>
2021-01-15 14:03:30 +0530

    String calculator first commit

---
+++ src/com/stringCalculator/StringCalculator.java
@@ -0,0 +1,27 @@
+package com.stringCalculator;
+
+public class StringCalculator {
+
+	public int add(String numbers) {
+	
+		if(numbers.equals(""))
+			return 0;
+		if(numbers.length() == 1)
+			return Integer.parseInt(String.valueOf(numbers.charAt(0)));
+
+		if(numbers.length() > 1 ){
+			String s[] = numbers.split(",");
+			int sum = 0;
+			for(int i=0;i<s.length;i++){
+				sum += Integer.parseInt(s[i]);
+			}
+			return sum;
+		}
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		StringCalculator s = new StringCalculator();
+		System.out.println(s.add("1,2,3"));
+	}
+}
